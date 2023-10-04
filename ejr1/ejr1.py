0293d238d9124344a6390acb663f5847d2583e39
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2023-10-04 09:53:05 +0200

    ejr1 bien

--- ejr1/ejr1.py
+++ ejr1/ejr1.py
@@ -1,56 +1,47 @@
-'''Creación de una clase en Python que representa una matriz.
-Para este ejercicio, deberás crear una clase que representa una matriz. Las operaciones que esta clase debe permitir son la creación de una matriz a partir de una lista de listas, la impresión de la matriz en una forma legible, y el cálculo de la transpuesta de la matriz. Asegúrate de que cada método tenga una única responsabilidad.'''
-
 class Matriz:
-    def __init__(self, elementos):
+    def __init__(self, elementos: list):
         self.elementos = elementos
 
-class Transpuesta(Matriz):
-    def __init__(self, elementos):
-        super().__init__(elementos)
-        
-    def transpuesta(self):
-        return Matriz([[fila[i] for fila in self.elementos] for i in range(len(self.elementos[0]))])
+class Transpuesta:
+    def __init__(self, matriz: Matriz):
+        self.matriz = matriz
     
-class Imprimir(Matriz):
-    def __init__(self, elementos):
-        super().__init__(elementos)
-        
+    def calcular_transpuesta(self):
+        return Matriz([[fila[i] for fila in self.matriz.elementos] for i in range(len(self.matriz.elementos[0]))])
+
+class Imprimir:
+    def __init__(self, matriz: Matriz):
+        self.matriz = matriz
+    
     def imprimir(self):
-        for fila in self.elementos:
+        for fila in self.matriz.elementos:
             print(fila)
 
-class Lanzador(Imprimir, Transpuesta):
+class Lanzador:
     def __init__(self):
         self.elementos = []
-        self.can_filas = int(input("Ingrese la cantidad de filas: "))
-        self.can_columnas = int(input("Ingrese la cantidad de columnas: "))
+        self.cantidad_filas = int(input("Ingrese la cantidad de filas: "))
+        self.cantidad_columnas = int(input("Ingrese la cantidad de columnas: "))
         self.crear_matriz()
-        super().__init__(self.elementos)
-    
+        self.matriz = Matriz(self.elementos)
+        self.transpuesta = Transpuesta(self.matriz)
+        self.imprimir = Imprimir(self.matriz)
+
     def crear_matriz(self):
-        for i in range(self.can_filas):
+        for i in range(self.cantidad_filas):
             fila = []
-            for j in range(self.can_columnas):
-                fila.append(int(input("Ingrese el elemento " + str(i) + " " + str(j) + ": ")))
+            for j in range(self.cantidad_columnas):
+                fila.append(int(input(f"Ingrese el elemento {i+1},{j+1}: ")))
             self.elementos.append(fila)
-    
+
     def lanzar(self):
         print("La matriz es: ")
-        self.imprimir()
-        print("La transpuesta es: ")
-        self.transpuesta()
-        self.imprimir()
-
-
-
-class Main:
-    def __init__(self):
-        self.lanzador = Lanzador()
-        self.lanzador.lanzar()
+        self.imprimir.imprimir()
+        print("La matriz transpuesta es: ")
+        transpuesta_result = self.transpuesta.calcular_transpuesta()
+        imprimir_transpuesta = Imprimir(transpuesta_result)
+        imprimir_transpuesta.imprimir()
 
 if __name__ == "__main__":
-    Main()
-
-
-
+    lanzador = Lanzador()
+    lanzador.lanzar()
